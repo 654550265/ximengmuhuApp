dcc1647a90a82fed4274b08c87b46de6fa7ae556
lianghiu <p7s0wyw@dingtalk.com>
2017-05-16 16:23:15 +0800

    创建lianghiSCSS文件

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,11 +2,13 @@ angular.module('starter.controllers', [])
     .controller('MessageCtrl', function ($scope) {
 
     })
-
-    .controller('FarmCtrl', function ($scope) {
-
+    .controller('FarmCtrl', function ($scope,$state) {
+        if(window.localStorage.firstrun==="false"){
+            $state.go("login");
+        }
     })
-
+  .controller('loginCtrl', function ($scope) {
+  })
     .controller('ServiceCtrl', function ($scope) {
     })
     .controller('AccountCtrl', function ($scope) {
