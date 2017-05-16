da341539c2c4995888da31255c5b2cab1274516b
USER-NHLVP4O4TL\Administrator <luker.springtree@foxmail.com>
2017-05-16 15:08:06 +0800

    1. 实现基本导航 2.修改 头样式

--- www/js/app.js
+++ www/js/app.js
@@ -1,67 +1,60 @@
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
-  .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  })
-  .state('login', {
-    url: "/login",
-    templateUrl: 'pages/login/login.html',
-    controller: 'loginCtrl'
-  });
-  $urlRouterProvider.otherwise('/tab/dash');
 
-});
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('tab', {
+                url: '/tab',
+                abstract: true,
+                templateUrl: 'templates/tabs.html'
+            })
+            .state('tab.message', {
+                url: '/message',
+                views: {
+                    'tab-message': {
+                        templateUrl: 'templates/tab-message.html',
+                        controller: 'MessageCtrl'
+                    }
+                }
+            })
+            .state('tab.farm', {
+                url: '/farm',
+                views: {
+                    'tab-farm': {
+                        templateUrl: 'templates/tab-farm.html',
+                        controller: 'FarmCtrl'
+                    }
+                }
+            })
+            .state('tab.service', {
+                url: '/service',
+                views: {
+                    'tab-service': {
+                        templateUrl: 'templates/tab-service.html',
+                        controller: 'ServiceCtrl'
+                    }
+                }
+            })
+            .state('tab.account', {
+                url: '/account',
+                views: {
+                    'tab-account': {
+                        templateUrl: 'templates/tab-account.html',
+                        controller: 'AccountCtrl'
+                    }
+                }
+            });
+        $urlRouterProvider.otherwise('/tab/farm');
+
+    });
