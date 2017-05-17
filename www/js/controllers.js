77cf5ba80471096d2361653236d6fd67cfa34e17
USER-NHLVP4O4TL\Administrator <luker.springtree@foxmail.com>
2017-05-17 10:19:36 +0800

    1. 完成前端界面!

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,13 +2,13 @@ angular.module('starter.controllers', [])
     .controller('MessageCtrl', function ($scope) {
 
     })
-    .controller('FarmCtrl', function ($scope,$state) {
-        if(window.localStorage.firstrun==="false"){
+    .controller('FarmCtrl', function ($scope, $state) {
+       /* if (window.localStorage.firstrun === "false") {
             $state.go("login");
-        }
+        }*/
     })
-  .controller('loginCtrl', function ($scope) {
-  })
+    .controller('loginCtrl', function ($scope) {
+    })
     .controller('ServiceCtrl', function ($scope) {
     })
     .controller('AccountCtrl', function ($scope) {
